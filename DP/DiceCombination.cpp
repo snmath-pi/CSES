29f6bb1da510b928614a77ef500b162e7748a1f6
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-02-01 10:31:12 +0530

    Create DiceCombination.cpp

---
+++ DP/DiceCombination.cpp
@@ -0,0 +1,26 @@
+/*亲爱的上帝，我求你给我力量来赢得这场战斗。*/
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+
+const int mxN = 1000010;
+i64 dp[mxN];
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int N;
+    cin >> N;
+    constexpr int MOD = 1e9 + 7;
+    dp[0] = 1;
+    for(int i=1; i<=N; i++) {
+        // dp[i] -> the number of ways to form sum i
+        // we have choices uptil min(i, number on the die);
+        for(int j=1; j<=min(i, 6); j++) {
+            (dp[i] += dp[i-j]) %= MOD;
+        }
+    }
+    cout << dp[N];
+    
+    return 0;
+}
